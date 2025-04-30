be163e9d0f6b5e554d6583f3e409261930fdbdd0
gabryel <gabryelaraujo71@gmail.com>
2025-04-30 14:25:12 -0300

    feat: criação do componente menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,73 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type avaliableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<avaliableThemes>(() => {
+    const theme = (localStorage.getItem("theme") as avaliableThemes) || "dark";
+    return theme;
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  function handleChangeTheme(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    e.preventDefault();
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  }
+
+  const ThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para a home"
+        title="Home"
+      >
         <HouseIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ver histórico"
+        title="Histórico"
+      >
         <HistoryIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para as configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
-        <SunIcon />
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={handleChangeTheme}
+      >
+        {ThemeIcon[theme]}
       </a>
     </nav>
   );
