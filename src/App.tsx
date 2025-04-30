be06de8546edd13552c5d99ba748793d6e7fc895
gabryel <gabryelaraujo71@gmail.com>
2025-04-30 14:22:36 -0300

    feat: implementação do componente home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
-import { Container } from "./components/Container";
-import { CountDown } from "./components/CountDown";
-import { Form } from "./components/Form";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
+import { Home } from "./pages/Home";
 
-function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <Form />
-      </Container>
-    </>
-  );
+export function App() {
+  return <Home />;
 }
-
-export default App;
