d4288a67daa1047909507f68cf0eb7732a2b8072
gabryel <gabryelaraujo71@gmail.com>
2025-04-29 23:48:08 -0300

    feat: criação do componente input

---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+
+type DefaultInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<"input">;
+
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
+  return (
+    <>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input type={type} id={id} {...rest} className={styles.input} />
+    </>
+  );
+}
