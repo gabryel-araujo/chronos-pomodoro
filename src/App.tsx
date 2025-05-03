3aa54ccc0d70834f2fdf9aac2b3a20c35b29dd07
gabryel <gabryelaraujo71@gmail.com>
2025-05-03 16:39:22 -0300

    feat: implementação do contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { Home } from "./pages/Home";
-import { TaskContextProvider } from "./contexts/TaskContext";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedsecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
   return (
     <TaskContextProvider>
       <Home />
