2cf1e5251f63f004960ec32e2178ebf64df29a38
gabryel <gabryelaraujo71@gmail.com>
2025-05-10 14:03:14 -0300

    feat: ajuste na classe do botão

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -3,11 +3,13 @@ import styles from "./styles.module.css";
 type DefaultButtonProps = {
   icon: React.ReactNode;
   color?: "green" | "red";
+  square?: boolean;
 } & React.ComponentProps<"button">;
 
 export function DefaultButton({
   color = "green",
   icon,
+  square,
   ...props
 }: DefaultButtonProps) {
   return (
@@ -16,7 +18,7 @@ export function DefaultButton({
         {...props}
         className={`${styles.button} ${
           color === "green" ? styles.green : styles.red
-        }`}
+        } ${square && styles.square}`}
       >
         {icon}
       </button>
