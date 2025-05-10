7bebe444d26d41c27665a63a0848a3661054684b
gabryel <gabryelaraujo71@gmail.com>
2025-05-10 15:41:32 -0300

    feat: adicionado a paginação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
+import { History } from "./pages/History";
 import { Home } from "./pages/Home";
+import { pageContext } from "./contexts/PageContext/pageContext";
 
 export function App() {
+  const { page } = useContext(pageContext);
+
   return (
     <TaskContextProvider>
-      <Home />
+      {page === "main" && <Home />}
+      {page === "history" && <History />}
     </TaskContextProvider>
   );
 }
