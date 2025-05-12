e49d5ff117a20b662e26bf6d6b6b74c29a607e43
gabryel <gabryelaraujo71@gmail.com>
2025-05-12 12:25:35 -0300

    feat: instanciação do toaster

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import "./styles/theme.css";
 import "./index.css";
 import { App } from "./App";
 import { PageContextProvider } from "./contexts/PageContext/PageContextProvider";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PageContextProvider>
+      <Toaster richColors position="top-center" />
       <App />
     </PageContextProvider>
   </StrictMode>
